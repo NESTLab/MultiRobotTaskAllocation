c6b76b90aab557b3abcf26b93d8591ef9cf93081
AshayAswale <asaswale@wpi.edu>
2024-03-04 18:53:29 -0500

    Moved updateMrtaConfig to MrtaGenericSolver

--- solvers/include/mrta_solvers/mrta_heuristic_solver.h
+++ solvers/include/mrta_solvers/mrta_heuristic_solver.h
@@ -1,5 +1,3 @@
-#include <eigen3/Eigen/Dense>
-#include <eigen3/unsupported/Eigen/CXX11/Tensor>
 #include <mrta_solvers/mrta_generic_solver.h>
 #include <mrta_utilities/mrta_config.h>
 
@@ -31,72 +29,14 @@ public:
   }
 
 private:
-  int START_ID = 0;
-  int END_ID = 0;
   HEURISTIC_SOLVER_CONFIG heuristic_method_config;
   void solveMrtaProblem(const MrtaConfig::CompleteConfig &mrta_complete_config,
                         MrtaSolution::CompleteSolution &ret_complete_solution);
 
-  void
-  updateMrtaConfig(const MrtaConfig::CompleteConfig &mrta_complete_config_in) {
-    config_initialized = true;
-    mrta_complete_config = &mrta_complete_config_in;
-    END_ID = mrta_complete_config->setup.number_of_destinations - 1;
-    robot_task_id_attendance_sequence.resize(
-        mrta_complete_config->setup.number_of_robots);
-    for (auto &robot_task_att : robot_task_id_attendance_sequence) {
-      robot_task_att = std::vector<int>(1, START_ID);
-    }
-    initializeDistanceTensor();
-    task_start_time = std::vector<double>(
-        mrta_complete_config->setup.number_of_destinations, 0.0);
-    initializeTaskRequirementMatrix(mrta_complete_config_in);
-  };
-
-  void initializeTaskRequirementMatrix(
-      const MrtaConfig::CompleteConfig &mrta_complete_config_in) {
-    int number_of_destinations =
-        mrta_complete_config_in.setup.number_of_destinations;
-    task_requirements_matrix = Eigen::MatrixXd::Zero(
-        mrta_complete_config_in.setup.number_of_robots,
-        mrta_complete_config_in.setup.number_of_destinations);
-    for (int skill_id=0; skill_id<mrta_complete_config->setup.all_skill_names.size(); skill_id++) {
-      std::string skill_name = mrta_complete_config->setup.all_skill_names.at(skill_id);
-      for (int j = START_ID + 1;           // Skipping the START task
-           j < number_of_destinations - 1; // Skipping the END task
-           ++j) {
-        std::map<std::string, MrtaConfig::Task>::const_iterator task_info_itr =
-            mrta_complete_config_in.tasks_map.find(
-                mrta_complete_config_in.setup.all_destination_names.at(j));
-        std::map<std::string, double>::const_iterator task_skill_itr =
-            task_info_itr->second.skillset.find(skill_name);
-        task_requirements_matrix(j, skill_id) = task_skill_itr->second;
-      }
-      task_requirements_matrix(START_ID, skill_id) = 0;
-      task_requirements_matrix(END_ID, skill_id) = 0;
-    }
-  }
-
-  bool config_initialized = false;
-  MrtaConfig::CompleteConfig const *mrta_complete_config;
-
   Eigen::MatrixXd contribution_array;
-  Eigen::MatrixXd task_requirements_matrix;
-  std::vector<Eigen::MatrixXd> robot_distances_vector;
 
-  std::vector<std::vector<int>> robot_task_id_attendance_sequence;
-  std::vector<double> task_start_time;
-  std::map<std::string, std::map<std::string, double>>
-      robot_task_attendance_times_map;
 
   void updateWorldStatus(){};
-
-  void initializeDistanceTensor();
-
-  void putDistancesForRobot(int robot_id,
-                            Eigen::MatrixXd &ret_i_distance_matrix);
-  double getPureDistance(const MrtaConfig::Position &task_1_position,
-                         const MrtaConfig::Position &task_2_position);
 
   void updateContributionsFromConfig();
 
--- solvers/mrta_generic_solver.cpp
+++ solvers/mrta_generic_solver.cpp
@@ -1,2 +1,65 @@
 #include <mrta_solvers/mrta_generic_solver.h>
 
+
+void MrtaGenericSolver::initializeDistanceTensor() {
+  int number_of_robots = mrta_complete_config->setup.number_of_robots;
+  int number_of_destinations =
+      mrta_complete_config->setup.number_of_destinations;
+  robot_distances_vector.resize(number_of_robots);
+  for (size_t i = 0; i < number_of_robots; i++) {
+    robot_distances_vector.at(i) =
+        Eigen::MatrixXd::Zero(number_of_destinations, number_of_destinations);
+    putDistancesForRobot(i, robot_distances_vector.at(i));
+  }
+}
+
+void MrtaGenericSolver::putDistancesForRobot(
+    int robot_id, Eigen::MatrixXd &ret_i_distance_matrix) {
+  int number_of_destinations =
+      mrta_complete_config->setup.number_of_destinations;
+
+  // FIRST and LAST destinations are START and END, hence skipping them
+  for (size_t task_id = START_ID + 1; task_id < number_of_destinations - 1;
+       task_id++) {
+    std::map<std::string, MrtaConfig::Robot>::const_iterator robot_itr =
+        mrta_complete_config->robots_map.find(
+            mrta_complete_config->setup.all_robot_names.at(robot_id));
+    std::map<std::string, MrtaConfig::Task>::const_iterator task_itr =
+        mrta_complete_config->tasks_map.find(
+            mrta_complete_config->setup.all_destination_names.at(task_id));
+
+    ret_i_distance_matrix(START_ID, task_id) =
+        getPureDistance(robot_itr->second.position, task_itr->second.position);
+    ret_i_distance_matrix(END_ID, task_id) = getPureDistance(
+        robot_itr->second.desired_end_position, task_itr->second.position);
+    ret_i_distance_matrix(task_id, START_ID) =
+        ret_i_distance_matrix(START_ID, task_id);
+    ret_i_distance_matrix(task_id, END_ID) =
+        ret_i_distance_matrix(END_ID, task_id);
+    ret_i_distance_matrix(START_ID, START_ID) =
+        std::numeric_limits<double>::infinity();
+    ret_i_distance_matrix(END_ID, END_ID) =
+        std::numeric_limits<double>::infinity();
+    ret_i_distance_matrix(task_id, task_id) =
+        std::numeric_limits<double>::infinity();
+    for (size_t second_task_id = task_id + 1;
+         second_task_id < number_of_destinations - 1; second_task_id++) {
+      std::map<std::string, MrtaConfig::Task>::const_iterator second_task_itr =
+          mrta_complete_config->tasks_map.find(
+              mrta_complete_config->setup.all_destination_names.at(
+                  second_task_id));
+      ret_i_distance_matrix(task_id, second_task_id) = getPureDistance(
+          task_itr->second.position, second_task_itr->second.position);
+      ret_i_distance_matrix(second_task_id, task_id) =
+          ret_i_distance_matrix(task_id, second_task_id);
+    }
+  }
+}
+
+double MrtaGenericSolver::getPureDistance(
+    const MrtaConfig::Position &task_1_position,
+    const MrtaConfig::Position &task_2_position) {
+  double x_diff = task_1_position.pos_x - task_2_position.pos_x;
+  double y_diff = task_1_position.pos_y - task_2_position.pos_y;
+  return std::sqrt(std::abs(x_diff * x_diff + y_diff * y_diff));
+}
